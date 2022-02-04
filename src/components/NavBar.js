fe116c4181a14d3087f82462222e407e0ca673ac
suman <sumall@utu.fi>
2022-02-05 01:43:10 +0200

    starting with routing

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Nav Components </h1>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/pages/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../NavBar'
+import Home from './Home'
+
+function Layout() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  )
+}
+
+export default Layout
